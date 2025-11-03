e3ac9e950f45adc7666e8dd4b280f97659ebf5c7
Brian Kwong <bkwong01@calpoly.edu>
2025-11-03 14:58:17 -0800

    Updated to ssg

--- packages/brian-kwong/src/main.tsx
+++ packages/brian-kwong/src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import { ViteReactSSG } from 'vite-react-ssg/single-page';
 
-createRoot(document.getElementById("root")!).render(
+export const createRoot = ViteReactSSG(
     <StrictMode>
         <App />
     </StrictMode>
-);
+)
