9aab6d55fe81a8b8ccf7d882b4ea1dbe7114fecf
Brian Kwong <bkwong01@calpoly.edu>
2025-11-03 21:24:02 -0800

    Update main.tsx

--- packages/brian-kwong/src/main.tsx
+++ packages/brian-kwong/src/main.tsx
@@ -3,6 +3,10 @@ import "./index.css";
 import App from "./App.tsx";
 import { ViteReactSSG } from "vite-react-ssg/single-page";
 
+if (typeof document !== 'undefined') {
+  document.documentElement.lang = 'en';
+}
+
 export const createRoot = ViteReactSSG(
     <StrictMode>
         <App />
